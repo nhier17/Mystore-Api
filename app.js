50d00489bdf76b3a2c2f5760bdba2f66e2738341
Abraham <abrahamnhier97@gmail.com>
2024-01-22 09:20:33 +0300

    connected to db with mongoose

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ require('dotenv').config();
 
 const express = require('express');
 const app = express();
+
+const connectDB = require('./db/connect')
 const notFoundMiddleware = require('./middleware/notFound')
 const errorHandler = require('./middleware/error-handler')
 
@@ -19,6 +21,7 @@ const port = process.env.PORT || 3001
 
 const start = async () => {
     try {
+        await connectDB(process.env.MONGO_URI)
 app.listen(port, console.log("server is listening on port 3001..."))
     } catch (error) {
 console.log(error)
