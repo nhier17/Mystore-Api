44de727d55e9c30baff04428df453b9fa259ec86
Abraham <abrahamnhier97@gmail.com>
2024-01-22 08:49:33 +0300

    set up express

---
+++ app.js
@@ -0,0 +1,27 @@
+require('dotenv').config();
+
+const express = require('express');
+const app = express();
+const notFoundMiddleware = require('./middleware/notFound')
+const errorHandler = require('./middleware/error-handler')
+
+
+app.use(express.json());
+
+app.get('/', (req,res) => {
+    res.send("<h1>Store Api</h1><a href='/api/v1/products'>products</a>")
+})
+
+app.use(notFoundMiddleware)
+app.use(errorHandler)
+
+const port = process.env.PORT || 3001
+
+const start = async () => {
+    try {
+app.listen(port, console.log("server is listening on port 3001..."))
+    } catch (error) {
+console.log(error)
+    }
+}
+start()
